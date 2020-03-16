74e96b59cceebff557f67ec2481400bdccb31643
Jakob Wagner <jakob@winter-wind.de>
2020-03-16 19:22:58 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+import { CookingComponent } from './cooking/cooking.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'cooking', component: CookingComponent},
+  { path: 'contact', component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [ ProjectsComponent, HomeComponent ]
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
